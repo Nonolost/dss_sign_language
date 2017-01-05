dce3fb77ec66fa49ee8aeb067184bcf43974b234
Arnaud Lods <arnaud.lods@gmail.com>
2017-01-05 11:02:12 +0100

    new method with sax derivate, now manage which method to use with command line, function are now commented, few update to the previous code

--- symbolic.py
+++ symbolic.py
@@ -1,18 +1,22 @@
 import os
 import numpy as np
-# This file contains the three methods that will allow to go from numerical data to symbolic data
+import sys
+# This file contains the 4 methods that will allow to go from numerical data to symbolic data
 
-# TODO : lets make sax on derivate to get something looking like the home-made
-# New parameter for sax : vary the distance between two horizontal separation in sax/set-based
-# TODO : function that call every function with different parameters
-# TODO : SAX pour les doigts faire avec le nb de valeurs différentes, pour x y z essayer de trouver des positions clés pour séparer là, et orientation main?
+# TODO : New parameter for sax : vary the distance between two horizontal separation in sax/set-based
 
+index_to_attribute = {0:'x', 1:'y', 2:'z', 3:"palm roll", 4:"thumb", 5:"index", 6:"middle", 7:"ring"}
 
-
-# The home made function take into argument a file with numerical data and will convert it into symbolic data in an output file
-# In this version, we will take values 3 by 3 and if these values varied outside of a defined threshold, we apply the I (incrementation), D (decrementation).
-# If not, we label it C (Constant)
 def home_made(input_file, output_file, gap):
+	"""
+	This "home made" function transform a numerical sequence into a symbolic one. For a value it takes both its previous and next value to calculate the variation
+	If this result is greater than a gap defined in the parameters, it will label the value as either I (incrementation) or D (decrementation).
+	If this result is lesser than the gap, then the valus is labeled C (constant)
+
+	input_file: file from where the data is read
+	output_file: file where the data is written
+	gap: value of the gap to consider
+	"""
 	dict_values = from_file_to_dict(input_file)
 
 	new_dict_values = dict()
@@ -32,8 +36,16 @@ def home_made(input_file, output_file, gap):
 
 	from_dict_to_file(output_file, new_dict_values)
 
-# Follozwing SAX method
 def sax(input_file, output_file, height):
+	"""
+	This function is a simplified use of the SAX method to transform numerical data into symbolic one. 
+	See http://cs.gmu.edu/~jessica/SAX_DAMI_preprint.pdf for details
+	Right now all separation are equally distant
+
+	input_file: file from where the data is read
+	output_file: file where the data is written
+	height: number of horizontal separation
+	"""
 	dict_values = from_file_to_dict(input_file)
 
 	gap = 2/height
@@ -45,8 +57,56 @@ def sax(input_file, output_file, height):
 
 	from_dict_to_file(output_file, dict_values)
 
-# Following set based method
+
+def sax_derivate(input_file, output_file, height):
+	"""
+	This function is similar to the sax one, but here we use SAX on the derivate of the values (for a value, variance between the previous and next values)
+
+	input_file: file from where the data is read
+	output_file: file where the data is written
+	height: number of horizontal separation
+	"""
+	dict_values = from_file_to_dict(input_file)
+
+	letters = ['A','B','C','D','E','F','G','H','I','J','K']
+
+	new_dict_values = dict()
+
+	for i in range(0,8):
+		new_dict_values[i] = list()
+
+	for key, values in dict_values.items():
+		der = []
+		if key == 3:
+			print(values)
+		for idx, value in enumerate(values):
+			if idx != 1 and idx != len(values)-1:
+				der.append(values[idx+1]-values[idx-1])
+		
+		if key == 3:
+			print(der)
+		gap = (max(der)-min(der))/height
+
+		for idx, v in enumerate(der):
+			if gap==0: # that's when there is not variation = we don't care about the letter so let's just put a A
+				new_dict_values[key].append(letters[0])
+			else:
+				new_dict_values[key].append(letters[int((abs(min(der))+v)/gap)])
+
+	from_dict_to_file(output_file, new_dict_values)
+
+
 def set_based(input_file, output_file, height, width):
+	"""
+	This function is a simplified use of the Set-based method to transform numerical data into symbolic one. 
+	See http://dl.acm.org/citation.cfm?id=2882963 for details
+	Right now all 'set' have the same height and width
+
+	input_file: file from where the data is read
+	output_file: file where the data is written
+	height: number of horizontal separation
+	width: number of vertical separation
+	"""
 	dict_values = from_file_to_dict(input_file)
 
 	height_gap = 2/height
@@ -59,50 +119,66 @@ def set_based(input_file, output_file, height, width):
 
 	from_dict_to_file(output_file, dict_values)
 
-# This function take into argument a file, will read its content and transfer data into a dictionnary
+
 def from_file_to_dict(input_file):
+	"""
+	This function take a file in argument and will write its content into a dictionnart to easy the data management
+
+	input_file: name of the file to read the data from
+	"""
 	file = open(input_file)
 
-	dict_values = dict()
-
-	for i in range(0,8):
-		dict_values[i] = list()
+	dict_values = {k: [] for k in range(8)}
 
 	for line in file:
 		s = line.split(" ")
+		s.pop(0) # first column only indicate line's number
+		s.remove('\n')
 		for idx, a in enumerate(s):
-			if idx != 0 and idx<9:
-				try:
-					dict_values[idx-1].append(float(a))
-				except ValueError:
-					pass
+			dict_values[idx].append(float(a))
+
 	file.close
 
 	return dict_values
-# This function take into argument a dictionnary and will write its content into a file
-def from_dict_to_file(output_file, values):
+
+def from_dict_to_file(output_file, dict_values):
+	"""
+	This function take a dictionnary of values in argument and will write its content into a file respecting the way the data files are constructed
+
+	output_file: name of the file to write the data into
+	dict_values: values to write in the file
+	"""
 	file = open(output_file, 'w')
 
-	for i in range(0,len(values[0])):
+	for i in range(0,len(dict_values[0])):
 		line = ""
-		for key, value in values.items():
+
+		for key, value in dict_values.items():
 			line += str(value[i]) + " "
 		file.write(str(i) + " " + line + "\n")
 
 	file.close
 
 if __name__ == "__main__":
-    # Here we're going to generate all the data for sax, derivated sax, set-based and home-made for all our dataset and for different parameters
+	"""
+	The script is used as follow : python symbolic.py [sax] [sax_der] [home] [set] depending of whichever kind of method you want to use
+	"""
+	if len(sys.argv) > 1:
+		for file in os.listdir("data"):
+			if file.endswith(".dat"):
+				for i in range(1,6):
+					if "sax" in sys.argv:
+						sax("data/" + file, "data/sax/" + file.replace(".dat", "") + "_sax_" + str(i) + ".dat", i)
+					if "sax_der" in sys.argv:
+						sax_derivate("data/" + file, "data/sax_derivate/" + file.replace(".dat", "") + "_sax_derivate_" + str(i) + ".dat", i)
 
-    # sax :	
-	for file in os.listdir("data"):
-		if file.endswith(".dat"):
-			for i in range(1,6):
-				sax("data/" + file, "data/sax/" + file.replace(".dat", "") + "_sax_" + str(i) + ".dat", i)
+				if "home" in sys.argv:
+					for i in np.arange(0.05,0.5,0.05):
+						home_made("data/" + file,  "data/hm/" + file.replace(".dat", "") + "_hm_" + str(i) + ".dat", i)
 
-			for i in np.arange(0.05,0.5,0.05):
-				home_made("data/" + file,  "data/hm/" + file.replace(".dat", "") + "_hm_" + str(i) + ".dat", i)
-
-			for i in range(1,5):
-				for j in range(1,5):
-					set_based("data/" + file,  "data/set_based/" + file.replace(".dat", "") + "_setbased_" + str(i) + "_" + str(j) + ".dat", i, j)
+				if "set" in sys.argv:
+					for i in range(1,5):
+						for j in range(1,5):
+							set_based("data/" + file,  "data/set_based/" + file.replace(".dat", "") + "_setbased_" + str(i) + "_" + str(j) + ".dat", i, j)
+	else:
+		print("Too few arugments, enter at least one of those : python symbolic.py [sax] [sax_der] [home] [set]")
